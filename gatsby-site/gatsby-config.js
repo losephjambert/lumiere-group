f67767caa6a2a29e7b0042d03c70370731b36d9d
losephjambert <josephc.lambert@gmail.com>
2018-06-13 23:23:39 -0700

    establishes contentful connection

--- gatsby-site/gatsby-config.js
+++ gatsby-site/gatsby-config.js
@@ -1,6 +1,16 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Lumiere Group',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `e7ih4eho2020`,
+        accessToken: `5a3fbd9650d5c6cc3771bf673b9a272ce9fb5398f4efaa729e7bdd024d4d3804`,
+      },
+    },
+  ],
+  
 }
--- gatsby-site/src/pages/index.js
+++ gatsby-site/src/pages/index.js
@@ -1,13 +1,66 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import Img from "gatsby-image"
 
-const IndexPage = () => (
+const Profile = ({data}) => (
   <div>
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <Link to="/page-2/">Go to page 2</Link>
+    <div>
+      <div
+        style={{
+          display: `flex`,
+          alignItems: `center`
+        }}
+      >
+        <div>
+          {data.profilePhoto.resolutions.src && (
+            <Img
+              style={{ margin: 0 }}
+            />
+          )}
+        </div>
+        <div style={{ flex: 1 }}>{data.name}</div>
+        <div style={{ flex: 1 }}>{data.profileDescription.profileDescription}</div>
+      </div>
+    </div>
   </div>
 )
 
+
+class IndexPage extends React.Component{
+  render(){
+    const data = this.props.data.allContentfulTest.edges[0].node;
+    console.log(data);
+  
+    return(
+      <Profile data={data}/>
+    )
+
+  }
+}
+
 export default IndexPage
+
+export const pageQuery = graphql`
+  query PageQuery {
+    allContentfulTest {
+      edges {
+        node {
+          id
+          name
+          profilePhoto{
+            resolutions{
+              src
+            }
+          }
+          profileDescription{
+            profileDescription
+          }
+        }
+      }
+    }
+  }
+`
