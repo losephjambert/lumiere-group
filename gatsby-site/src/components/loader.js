afc3e550f3177aacfbc7861bf5a289c7a6d5edd0
Joe Lambert <josephc.lambert@gmail.com>
2018-07-17 22:45:04 -0700

    lottie round 4

--- gatsby-site/src/components/loader.js
+++ gatsby-site/src/components/loader.js
@@ -1,28 +1,31 @@
 import React from 'react'
 import Styled, {css} from 'styled-components'
 import Lottie from 'react-lottie';
-import * as animationData from '../assets/rotating-logo-2000x2000-smaller.json'
+import * as animationData from '../assets/rotating-logo-2000x2000-smaller-centered.json'
 
 const Container = Styled.div`
   position: fixed;
-  top: -15rem;
+  top: 0;
   left: 0;
+  right: 0;
   bottom: 0;
   width: 100%;
-  transition: 500ms;
   display: flex;
   justify-content: center;
-  align-items; center;
+  align-items: center;
   z-index: 500;
   background-color: rgba(255,255,255,1);
   transition: 200ms;
   ${props=>props.isStopped && css`
-    background-color: rgba(255,255,255,0);
-    z-index: 10;
+  background-color: rgba(255,255,255,0);
+  z-index: 10;
   `}
-`
-
-const LottieContainer = Styled(Lottie)`
+  `
+  
+const LottieContainer = Styled.div`
+  width: 100%;
+  height: 100%;
+  // transform: translate3d(0,-15rem,0);
 `
 
 class Loader extends React.Component {
@@ -65,10 +68,12 @@ class Loader extends React.Component {
     
     return (
       <Container isStopped={this.state.isStopped} >
-        <LottieContainer options={defaultOptions}
-                eventListeners={eventListeners}
-                height={'105%'}
-                width={'100%'}/>
+        <LottieContainer>
+          <Lottie options={defaultOptions}
+                  eventListeners={eventListeners}
+                  height={'110%'}
+                  width={'100%'}/>
+        </LottieContainer>
       </Container>
     )
   }
