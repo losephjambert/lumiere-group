dbeda0009a9181114f51b447eeefc49489f642ea
Joe Lambert <josephc.lambert@gmail.com>
2020-08-23 06:05:28 -0700

    [fix] builds site, makes team rows variable, adds env vars, fixes some css issues.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,18 +1,49 @@
+require("dotenv").config({ path: `.env.${process.env.NODE_ENV}`, });
+
 module.exports = {
   siteMetadata: {
     title: 'Lumiere Group',
     footerTitle: 'Lumiere Group Footer Content',
   },
   plugins: [
-    'gatsby-plugin-react-helmet',
+    `gatsby-plugin-react-helmet`,
     `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-plugin-favicon`,
+      options: {
+        logo: './src/favicon.png',
+        appName: null, // Inferred with your package.json
+        appDescription: null,
+        developerName: null,
+        developerURL: null,
+        dir: 'auto',
+        lang: 'en-US',
+        background: '#fff',
+        theme_color: '#fff',
+        display: 'standalone',
+        orientation: 'any',
+        start_url: '/?homescreen=1',
+        version: '1.0',
+
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: true,
+          coast: false,
+          favicons: true,
+          firefox: true,
+          yandex: false,
+          windows: false
+        }
+      }
+    },
     {
       resolve: `gatsby-source-contentful`,
       options: {
-        spaceId: `e7ih4eho2020`,
-        accessToken: `5a3fbd9650d5c6cc3771bf673b9a272ce9fb5398f4efaa729e7bdd024d4d3804`,
+        spaceId: `${process.env.GATSBY_CONTENTFUL_SPACE_ID}`,
+        accessToken: `${process.env.GATSBY_CONTENTFUL_ACCESS_TOKEN}`,
       },
-    } ,
+    },
   ],
-  
+
 }
